10b6706413cd0447c10628f7a2512f356de3c6b6
sasapapovic988 <cutetiger0621@gmail.com>
2024-02-23 16:00:17 +0900

    connect wallet font size fixed

--- CoinDashboard-frontend/src/layouts/navbar.js
+++ CoinDashboard-frontend/src/layouts/navbar.js
@@ -27,7 +27,7 @@ const Navbar = () => {
                 <img src="/assets/icon/ic_cdbd.svg" />
                 <p className="text-xl font-semibold">Coin<span className="text-sky-700">Dashboard</span></p>
             </div>
-            <div className="hidden md:block  w-[370px] h-[40px] p-px overflow-hidden bg-gradient-to-b from-[#68F2C9] to-[#68F2C91C] rounded-3xl flex ">
+            <div className="hidden md:block  w-[370px] h-[40px] p-px overflow-hidden bg-gradient-to-b from-[#68F2C9] to-[#68F2C91C] rounded-3xl">
                 <div className="w-full h-full bg-[#04302EFF] flex flex-row gap-[28px] items-center justify-center">
                     <p className="text-sm font-medium cursor-pointer">Home</p>
                     <p className="text-sm font-medium cursor-pointer">Product</p>
@@ -36,7 +36,7 @@ const Navbar = () => {
                     <p className="text-sm font-medium cursor-pointer">FAQ</p>
                 </div>
             </div>
-            <button className="h-9 flex flex-row items-center justify-center rounded-3xl px-4 py-2 text-[14px] bg-cyan-500" onClick={onWalletConnect}>
+            <button className="h-9 flex flex-row items-center justify-center rounded-3xl px-4 py-2 text-[12px] bg-cyan-500" onClick={onWalletConnect}>
                 {!publicKey ? "Connect Wallet" : publicKey.toBase58().slice(0, 4) + " ... " + publicKey.toBase58().slice(-4)}
             </button>
         </div>
